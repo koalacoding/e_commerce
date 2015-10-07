5bc0a32698525650e911518501711fdf2efcc924
root <root@debian>
2015-10-08 00:36:58 +0100

    Working on index.php design.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include_once('include/session.php'); ?>
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
 <html>
 	<head>
 		<meta charset="utf-8" />
@@ -17,52 +17,52 @@
 				</div>
 
 				<div id="search_bar">
-					<img id="search_bar_glass" src="http://media.ldlc.com/v3/img/general/moteur/ico-loupe.svg" />
-					<input id="search_bar_field" type="search" name="search_field"/>
+					<div id="search_bar_glass_container">
+						<img id="search_bar_glass" src="http://media.ldlc.com/v3/img/general/moteur/ico-loupe.svg" />
+					</div>
+					<input id="search_bar_field" type="search" name="search_field" value="Rechercher"/>
+					<div id="search_bar_ok">OK</div>
 				</div>
 
-				<div id="right_buttons">
-					<center>
-					<?php
-						include_once ('sql/sql_connexion.php');
-						include_once('index_functions.php');
+				<div id="right_elements">
+					<div class="right_element">
+						<span class="element_logo">
+							<img src="http://media.ldlc.com/v3/img/general/ico-compte.png"
+							 alt="Mon compte" style="height:20px;width:20px;">
+						</span>
 
-						if (isset($_SESSION['username'])) { // If the user is connected to an account.
-							show_login_and_balance ($bdd, $_SESSION['username']);
-							show_connected_buttons($_SESSION['username']);
+						<div class="right_element_content">
+							<span class="element_title">Mon compte</span>
+							<span id="small_blue_arrow">&nbsp;</span>
+							<span class="element_text">Se connecter</span>
+						</div>
+					</div>
 
-							include_once('admin/check_if_user_is_admin/check_if_user_is_admin_function.php');
-							// If the user is admin.
-							if (is_user_admin($bdd, $_SESSION['username']) == TRUE) {
-								// Show the admin button.
-								echo '<a href="admin/index.php" class="button" id="admin_button"
-										class="button">Admin</a>';
-							}
-						}
+					<div class="right_element">
+						<span class="element_logo">
+							<img src="http://media.ldlc.com/v3/img/general/ico-compte.png"
+							 alt="Mon compte" style="height:20px;width:20px;">
+						</span>
 
-						else {
-							show_not_connected_buttons();
-						}
-					?>
-				</center>
+						<div class="right_element_content">
+							<span class="element_title">Mon panier</span>
+							<span id="small_blue_arrow">&nbsp;</span>
+							<span class="element_text">0 article&nbsp&nbsp&nbsp<span style="color: #ff0; font-weight: bold;">0,00 €</span></span>
+						</div>
+					</div>				
+
+					<div id="mon_panier">
+					</div>
 				</div>
+
 			</div>
 		</div>
 
 		<center>
 		<div id="core">
 			<div id="core_core">
-				<div id="core_core_core">
-					<img src="https://www.pinnaclesports.com/Cms_Data/Sites/Guest/Themes/Default/icons/icon-tick.png">
-					Ici le corps de la page.
-				</div>
-
-				<div id="menu">
-					<div id="menu_title">MENU</div>
-				</div>
-			</div>
-
-			
+				<div id="core_core_core">Ici le corps de la page.</div>
+			</div>	
 		</div>
 		</center>
 	</body>
